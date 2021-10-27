27609a0bd67047af2e2f61773ffba0f2f70e5fb9
Vitor <vit.riber1@hotmail.com>
2021-10-26 21:18:38 -0300

    feat/ajustando arquivos de rotas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
-const express = require('express')
+const express = require('express');
+const app = express();
+const routes = require ('./routes/routes');
 
-const app = express()
+app.use(express.json());
 
-app.get('*', (req,res) => {
-  res.send('Hello World!')
+app.use('/api', routes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
 })
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({error: error.message});
+})
+
 
 app.listen(process.env.PORT || '8080')
